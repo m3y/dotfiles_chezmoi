e7becd52508b761f2c8a7ad671f2658bbb2a63b1
m3y <ma3ya.ozw+github@gmail.com>
2026-01-26 19:21:00 +0900

    small update and add gh-markdown-preview

--- dot_config/nvim/lua/plugins/incline.lua
+++ dot_config/nvim/lua/plugins/incline.lua
@@ -13,6 +13,14 @@ return {
 					horizontal = 1,
 				},
 			},
+			render = function(props)
+				local buf = props.buf
+				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":t")
+				return {
+					vim.bo[buf].readonly and "RO " or "",
+					filename,
+				}
+			end,
 		})
 	end,
 	event = "VeryLazy",
