3edab7622556b61644544064c466c87545c5be6b
m3y <ma3ya.ozw+github@gmail.com>
2026-01-27 18:54:35 +0900

    update incline setting

--- dot_config/nvim/lua/plugins/incline.lua
+++ dot_config/nvim/lua/plugins/incline.lua
@@ -1,7 +1,13 @@
 return {
 	"b0o/incline.nvim",
-	config = function()
-		require('incline').setup({
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = function()
+		local devicons = require("nvim-web-devicons")
+
+		return {
 			window = {
 				placement = {
 					vertical = "bottom",
@@ -15,13 +21,25 @@ return {
 			},
 			render = function(props)
 				local buf = props.buf
-				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":t")
+				local bufname = vim.api.nvim_buf_get_name(buf)
+				local filename = vim.fn.fnamemodify(bufname, ":t")
+				if filename == "" then
+					filename = "[No Name]"
+				end
+
+				local icon, icon_color = devicons.get_icon_color(filename, nil, { default = true })
+
+				local modified = vim.bo[buf].modified
+				local readonly = vim.bo[buf].readonly or not vim.bo[buf].modifiable
+
 				return {
-					vim.bo[buf].readonly and "RO " or "",
-					filename,
+					{ icon, guifg = icon_color },
+					" ",
+					{ filename, gui = readonly and "italic" or nil },
+					modified and { " ●", guifg = "#e86671" } or nil, --modified mark
+					readonly and { " ", guifg = "#e5c07b" } or nil, -- lock mark
 				}
 			end,
-		})
+		}
 	end,
-	event = "VeryLazy",
 }
